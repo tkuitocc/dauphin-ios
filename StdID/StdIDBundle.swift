861e008fa2b6e4d3415e461b58ca5fe52a725ef7
u8b19 <bochainwu@gmail.com>
2025-04-22 13:35:31 +0800

    feat: Barcode widget

---
+++ StdID/StdIDBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StdIDBundle.swift
+//  StdID
+//
+//  Created by \u8b19 on 4/21/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StdIDBundle: WidgetBundle {
+    var body: some Widget {
+        StdID()
+    }
+}
