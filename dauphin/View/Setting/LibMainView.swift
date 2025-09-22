cd6d5d85a851bfbbe8f8eb2333d01030cbaf8370
OnCloud <anonymousaaaa41414141@gmail.com>
2025-09-22 23:01:52 +0800

    refactor: improve text display for student ID in LibMainView

--- dauphin/View/Setting/LibMainView.swift
+++ dauphin/View/Setting/LibMainView.swift
@@ -8,32 +8,32 @@
 import SwiftUI
 
 struct LibMainView: View {
-    @ObservedObject var viewModel: AuthViewModel
+  @ObservedObject var viewModel: AuthViewModel
 
-    var body: some View {
-        Group {
-            if viewModel.isLoggedIn {
-                VStack {
-                    if viewModel.isLoggedIn {
-                        Text("\(viewModel.ssoStuNo)")
-                            .padding()
-                    }
+  var body: some View {
+    Group {
+      if viewModel.isLoggedIn {
+        VStack {
+          if viewModel.isLoggedIn {
+            Text("Your student ID is \(viewModel.ssoStuNo)")
+              .padding()
+          }
 
-                    Button(action: {
-                        viewModel.logout()
-                    }) {
-                        Text("Log out")
-                            .foregroundColor(.red)
-                            .padding()
-                    }
-                }
-            } else {
-                LibSSOLoginView(viewModel: viewModel)
-            }
+          Button(action: {
+            viewModel.logout()
+          }) {
+            Text("Log out")
+              .foregroundColor(.red)
+              .padding()
+          }
         }
+      } else {
+        LibSSOLoginView(viewModel: viewModel)
+      }
     }
+  }
 }
 
 #Preview {
-    LibMainView(viewModel: AuthViewModel())
+  LibMainView(viewModel: AuthViewModel())
 }
