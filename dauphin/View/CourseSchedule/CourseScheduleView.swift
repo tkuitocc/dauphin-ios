d5df5a5e56193dc2394802fcd7c108cf49bbdf19
OnCloud <anonymousaaaa41414141@gmail.com>
2025-09-22 22:52:01 +0800

    refactor: adjust padding for course schedule view

--- dauphin/View/CourseSchedule/CourseScheduleView.swift
+++ dauphin/View/CourseSchedule/CourseScheduleView.swift
@@ -5,59 +5,61 @@
 //  Created by \u8b19 on 11/14/24.
 //
 
-import SwiftUI
 import Foundation
+import SwiftUI
 
 struct CourseScheduleView: View {
-    @Environment(\.horizontalSizeClass) var horizontalSizeClass
-    @ObservedObject var authViewModel: AuthViewModel
-    @StateObject var viewModel = CourseViewModel()
+  @Environment(\.horizontalSizeClass) var horizontalSizeClass
+  @ObservedObject var authViewModel: AuthViewModel
+  @StateObject var viewModel = CourseViewModel()
 
-    var body: some View {
-        Group {
-            if authViewModel.isLoggedIn {
-                if horizontalSizeClass == .compact {
-                    CourseScheduleByDayView(courseViewModel: viewModel, authViewModel: authViewModel)
-                        .refreshable {
-                            if !authViewModel.ssoStuNo.isEmpty {
-                                Task {
-                                    await viewModel.fetchCourses(with: authViewModel.ssoStuNo)
-                                }
-                            }
-                        }
-                        .onAppear {
-                            if !authViewModel.ssoStuNo.isEmpty {
-                                Task {
-                                    await viewModel.fetchCourses(with: authViewModel.ssoStuNo)
-                                }
-                            }
-                        }
-                } else {
-                    CourseScheduleByWeekView(courseViewModel: viewModel)
-                        .padding(20)
-                        .refreshable {
-                            if !authViewModel.ssoStuNo.isEmpty {
-                                Task {
-                                    await viewModel.fetchCourses(with: authViewModel.ssoStuNo)
-                                }
-                            }
-                        }
-                        .onAppear {
-                            if !authViewModel.ssoStuNo.isEmpty {
-                                Task {
-                                    await viewModel.fetchCourses(with: authViewModel.ssoStuNo)
-                                }
-                            }
-                        }
+  var body: some View {
+    Group {
+      if authViewModel.isLoggedIn {
+        if horizontalSizeClass == .compact {
+          CourseScheduleByDayView(courseViewModel: viewModel, authViewModel: authViewModel)
+            .refreshable {
+              if !authViewModel.ssoStuNo.isEmpty {
+                Task {
+                  await viewModel.fetchCourses(with: authViewModel.ssoStuNo)
                 }
-
-            } else {
-                AnyView(LibSSOLoginView(viewModel: authViewModel))
+              }
+            }
+            .onAppear {
+              if !authViewModel.ssoStuNo.isEmpty {
+                Task {
+                  await viewModel.fetchCourses(with: authViewModel.ssoStuNo)
+                }
+              }
+            }
+        } else {
+          CourseScheduleByWeekView(courseViewModel: viewModel)
+            .padding(
+              [.horizontal], 15
+            )
+            .refreshable {
+              if !authViewModel.ssoStuNo.isEmpty {
+                Task {
+                  await viewModel.fetchCourses(with: authViewModel.ssoStuNo)
+                }
+              }
+            }
+            .onAppear {
+              if !authViewModel.ssoStuNo.isEmpty {
+                Task {
+                  await viewModel.fetchCourses(with: authViewModel.ssoStuNo)
+                }
+              }
             }
         }
+
+      } else {
+        AnyView(LibSSOLoginView(viewModel: authViewModel))
+      }
     }
+  }
 }
 
 #Preview {
-    CourseScheduleView(authViewModel: AuthViewModel())
+  CourseScheduleView(authViewModel: AuthViewModel())
 }
