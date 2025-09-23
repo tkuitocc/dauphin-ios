4ecd8c7a2191effdc6a03352073f3f4518ea4481
u8b19 <bochainwu@gmail.com>
2025-09-24 07:43:54 +0800

    test: add initial test structure for ModelsTests and format code with swift-format

---
+++ ModelsTests/ModelsTests.swift
@@ -0,0 +1,16 @@
+//
+//  ModelsTests.swift
+//  ModelsTests
+//
+//  Created by \u8b19 on 9/24/25.
+//
+
+import Testing
+
+struct ModelsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
