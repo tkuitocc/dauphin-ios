401167fd510dab57f7f6362a694c86c3b8ca1822
u8b19 <bochainwu@gmail.com>
2025-09-24 14:09:27 +0800

    refactor: improve code readability and structure in CoursesNextUpViewLockScreenView

--- CoursesWidget/Views/NextUpLockScreenView.swift
+++ CoursesWidget/Views/NextUpLockScreenView.swift
@@ -20,56 +20,56 @@ struct CoursesNextUpViewLockScreenView: View {
           Color(UIColor.systemBackground)
         }
     } else {
-    }
-    if entry.courses.isEmpty {
-      Text("下週見")
-        .font(.caption2)
-        .padding()
-        .containerBackground(for: .widget) {
-          Color(UIColor.systemBackground)
-        }
-    } else {
-      HStack(alignment: .top) {
-        Rectangle()
-          .fill(Color.red)
-          .frame(width: 4)
-          .clipShape(RoundedRectangle(cornerRadius: 2, style: .continuous))
+      if entry.courses.isEmpty {
+        Text("下週見")
+          .font(.caption2)
+          .padding()
+          .containerBackground(for: .widget) {
+            Color(UIColor.systemBackground)
+          }
+      } else {
+        HStack(alignment: .top) {
+          Rectangle()
+            .fill(Color.red)
+            .frame(width: 4)
+            .clipShape(RoundedRectangle(cornerRadius: 2, style: .continuous))
 
-        Spacer()
+          Spacer()
 
-        VStack(alignment: .leading, spacing: 4) {
-          Text("\(entry.courses[0].name)")
-            .font(.system(size: 15, weight: .bold))
+          VStack(alignment: .leading, spacing: 4) {
+            Text("\(entry.courses[0].name)")
+              .font(.system(size: 15, weight: .bold))
 
-          Text(
-            "\(formatTime(entry.courses[0].startTime)) - \(formatTime(entry.courses[0].endTime))"
-          )
-          .font(.system(size: 12))
+            Text(
+              "\(formatTime(entry.courses[0].startTime)) - \(formatTime(entry.courses[0].endTime))"
+            )
+            .font(.system(size: 12))
 
-          HStack {
-            HStack(spacing: 0) {
-              Image(systemName: "location.circle")
-                .resizable()
-                .frame(width: 15, height: 15)
-              Text(" : \(entry.courses[0].room)")
-                .font(.system(size: 12))
-            }
+            HStack {
+              HStack(spacing: 0) {
+                Image(systemName: "location.circle")
+                  .resizable()
+                  .frame(width: 15, height: 15)
+                Text(" : \(entry.courses[0].room)")
+                  .font(.system(size: 12))
+              }
 
-            Spacer(minLength: 20)
+              Spacer(minLength: 20)
 
-            HStack(spacing: 0) {
-              Image(systemName: "graduationcap")
-                .resizable()
-                .frame(width: 15, height: 15)
-              Text(" : \(entry.courses[0].stdNo)")
-                .font(.system(size: 12))
+              HStack(spacing: 0) {
+                Image(systemName: "graduationcap")
+                  .resizable()
+                  .frame(width: 15, height: 15)
+                Text(" : \(entry.courses[0].stdNo)")
+                  .font(.system(size: 12))
+              }
             }
           }
         }
-      }
-      .padding(.vertical, 16)
-      .containerBackground(for: .widget) {
-        Color(UIColor.systemBackground)
+        .padding(.vertical, 16)
+        .containerBackground(for: .widget) {
+          Color(UIColor.systemBackground)
+        }
       }
     }
   }
