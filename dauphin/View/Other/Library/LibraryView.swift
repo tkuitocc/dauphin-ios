1876047a2a4b9ba7d65c7b232722d55ad6cfedc3
OnCloud <anonymousaaaa41414141@gmail.com>
2025-09-25 20:08:41 +0800

    feat(library): update library card UI
    This commit enhances the visual presentation of the library card by adopting a more modern, card-like design. The updated UI includes a subtle shadow effect, rounded corners, and improved text formatting for better readability and a cleaner aesthetic.

--- dauphin/View/Other/Library/LibraryView.swift
+++ dauphin/View/Other/Library/LibraryView.swift
@@ -5,33 +5,47 @@
 //  Created by \u8b19 on 11/25/24.
 //
 
-import SwiftUI
 import Code39
+import SwiftUI
 
 struct LibraryView: View {
-    @ObservedObject var authViewModel: AuthViewModel
-    var body: some View {
-        if authViewModel.isLoggedIn {
-            ScrollView{
-                VStack {
-                    VStack {
-                        Code39View("\(authViewModel.ssoStuNo)")
-                            .frame(width: 296, height: 96)
-                    }
-                    .padding(20)
-                    .background(Color.white)
-                    Text("stdID:\(authViewModel.ssoStuNo)")
-                        .padding(5)
-                }
-                .background(Color.accentColor)
-                .cornerRadius(10)
-            }
-        } else {
-            LibSSOLoginView(viewModel: authViewModel)
+  @ObservedObject var authViewModel: AuthViewModel
+  var body: some View {
+    if authViewModel.isLoggedIn {
+      VStack(spacing: 0) {
+        VStack(spacing: 0) {
+          VStack {
+            Code39View("\(authViewModel.ssoStuNo)")
+              .cornerRadius(0)
+              .frame(width: 296, height: 96)
+          }
+          .padding(20)
+          .background(Color.white)
+          Text("Student ID: \(authViewModel.ssoStuNo)")
+            .padding([.vertical], 8)
+            .foregroundColor(.white)
         }
+        .background(Color.accentColor)
+        .cornerRadius(16)
+        .background(
+          RoundedRectangle(cornerRadius: 16)
+            .shadow(
+              color: Color.black.opacity(0.2),
+              radius: 8,
+              x: 0,
+              y: 0
+            )
+        )
+
+        Spacer()
+      }
+
+    } else {
+      LibSSOLoginView(viewModel: authViewModel)
     }
+  }
 }
 
 #Preview {
-    LibraryView(authViewModel: AuthViewModel())
+  LibraryView(authViewModel: AuthViewModel())
 }
