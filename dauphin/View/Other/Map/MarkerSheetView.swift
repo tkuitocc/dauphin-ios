94147f98c547a7c8bccdd0760723a9020d55521d
cantpr09ram <bochainwu@gmail.com>
2025-11-10 19:04:29 +0800

    Add location list and enhance map marker selection
    Refactored L2GData to include id and code properties and made it Identifiable. Added LocationListView for displaying selectable locations. Updated MapView to use the new LocationListView in a sheet, improved marker selection with animation, and updated MarkerSheetView to display the location code.

--- dauphin/View/Other/Map/MarkerSheetView.swift
+++ dauphin/View/Other/Map/MarkerSheetView.swift
@@ -5,11 +5,14 @@ struct MarkerSheetView: View {
   let location: L2GData
 
   var body: some View {
-    VStack(alignment: .leading, spacing: 16) {
+    VStack(alignment: .leading, spacing: 10) {
       Text(location.name)
         .font(.title2)
         .fontWeight(.semibold)
         .frame(maxWidth: .infinity, alignment: .leading)
+      Text(location.code)
+        .font(.title3)
+        .fontWeight(.thin)
 
       LandmarkView(coordinate: location.coordinate)
     }
@@ -20,6 +23,7 @@ struct MarkerSheetView: View {
 #Preview("MarkerSheetView") {
   MarkerSheetView(
     location: L2GData(
+      code: "A",
       name: "書卷廣場",
       coordinate: CLLocationCoordinate2D(latitude: 25.17553, longitude: 121.45063)
     )
